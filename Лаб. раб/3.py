d3b73da7c38c0636ba7d8a5f4cd2b243d6f4b75d
KuzikDima <149532263+KuzikDima@users.noreply.github.com>
2023-10-31 22:36:23 +0300

    Add files via upload

---
+++ Лаб. раб/3.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+mid = len(list_players) // 2
+first = list_players[:mid]
+second = list_players[mid:]
+
+print(first)
+print(second)
+# TODO Разделите участников на две команды
